44ab89cf3c3d929a6d4becc4fc4678401e2a9df0
Sandro Sliskovic <sandro_sliskovic@sluz.ch>
2024-09-19 10:58:13 +0200

    linter test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser, // Zum Beispiel Browser-Globals, kann bei Bedarf angepasst werden
+      ecmaVersion: 2021, // Moderne ECMAScript-Version für ESM
+      sourceType: "module" // Wir verwenden ECMAScript Module (ESM)
+    },
+    rules: {
+      "no-unused-vars": "error", // Fehler für ungenutzte Variablen
+      "prefer-const": "error", // Erzwinge 'const' anstelle von 'let', wenn keine Zuweisung erfolgt
+      "eqeqeq": "error" // Erzwinge '===' anstelle von '=='
+    }
+  },
   pluginJs.configs.recommended,
 ];
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import chalk from 'chalk';
 
+let myUnusedVariable = 5;
+let abc
+if(abc == 3)
+
 console.log(chalk.blue('It works'));
+
