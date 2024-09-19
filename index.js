7ba24a7860337fd1e347564652b1e4f1b654ea7d
Sandro Sliskovic <sandro_sliskovic@sluz.ch>
2024-09-19 11:12:16 +0200

    errors fixed

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ console.log(chalk.blue('It works'));
 function calculate(a, b, operation)
 {
     if (operation === 'add') {
-        return a * b;
-      } else if (operation === 'subtract') {
         return a + b;
+      } else if (operation === 'subtract') {
+        return a - b;
       } else {
         throw new Error('Invalid operation');
       }
